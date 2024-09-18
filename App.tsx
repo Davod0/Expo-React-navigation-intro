2eb7b00973839bb317e901a063c03c3adf8ab6aa
Davod <davodnikoyii@gmail.com>
2024-09-18 12:10:43 +0200

    Added react navigation librarry and implemented two screens, Details and Home

--- App.tsx
+++ App.tsx
@@ -1,14 +1,49 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator, NativeStackScreenProps } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+};
+
+type HomeProps = NativeStackScreenProps<RootStackParamList, "Home">;
+
+const RootStack = createNativeStackNavigator<RootStackParamList>();
+
+
+export function HomeScreen({ navigation }: any) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Home screen</Text>
+      <Button title="Go to Details" onPress={() => navigation.navigate("Details")} />
       <StatusBar style="auto" />
     </View>
   );
 }
+
+export function DetailsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Details screen</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator initialRouteName="Home">
+        <RootStack.Screen name="Home" component={HomeScreen} />
+        <RootStack.Screen name="Details" component={DetailsScreen} />
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 
 const styles = StyleSheet.create({
   container: {
